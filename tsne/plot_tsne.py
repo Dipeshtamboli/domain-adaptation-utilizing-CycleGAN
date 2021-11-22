d388e3a5247ca130174d0e6172328093199e89ca
Dipeshtamboli <dipeshtamboli@gmail.com>
2021-11-22 15:37:43 -0500

    plot tsne

--- tsne/plot_tsne.py
+++ tsne/plot_tsne.py
@@ -13,8 +13,6 @@ start_time = time.time()
 # load all the npy feature vectors
 
 relative_path = 'datasets/resnet_features_complete_office31/'
-# relative_path = 'datasets/resnet_features_subset_office31/'
-# relative_path = 'datasets/office-31_10_class_subset/'
 
 all_npys = glob.glob(os.path.dirname(os.path.realpath(__file__))+'/'+relative_path+"**/*.npy" , recursive=True)
 
@@ -32,7 +30,6 @@ for i, npy_loc in enumerate(all_npys):
 
     if len(np.unique(class_names)) < num_plot_classes or class_name in class_names:
         if counter>= len(all_features):
-            # np.insert(all_features, counter, np.load(npy_loc))
             all_features = np.concatenate((all_features, np.load(npy_loc)), axis=0)
         else:
             all_features[counter] = np.load(npy_loc)
@@ -48,19 +45,11 @@ vis_y = embeddings[:, 1]
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 
 palette = sns.color_palette("bright", num_plot_classes)
-# palette = sns.color_palette("RdPu", 31)
 
-# pdb.set_trace()
-# plot = sns.scatterplot(vis_x, vis_y, hue=class_names, style = domain_names, markers=['P', 'o', 'X'], palette=palette)
 plot = sns.scatterplot(vis_x, vis_y, hue=class_names, style = domain_names, markers=['o'], palette=palette)
 plot.get_legend().set_title("Classes")
 
-# handles, labels = plot.get_legend_handles_labels()
-# labels[-1] = "gen"
-# labels[-2] = "conv"
-# plot.legend(handles, labels) 
 plot.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), ncol=1)
 plt.tight_layout()
 plt.savefig(f"TSNE_plots/office-31-dslr-{num_plot_classes}_classes_complete_dataset.png")
 print("--- {} mins {} secs---".format((time.time() - start_time)//60,(time.time() - start_time)%60))
-# pdb.set_trace()
